d2c3c3566cf3a36586a7431203c97b83df25190a
Lexey9105 <umpp120@gmail.com>
2023-04-13 19:56:41 +0300

    Сделал базувую и продвинутую часть02 -Исправления внесены

--- src/CourseWork.java
+++ src/CourseWork.java
@@ -3,6 +3,13 @@ import java.util.Arrays;
 
 public class CourseWork {
 
+    public static String StringToPrint(String workerPrint){
+        workerPrint=workerPrint.replace("null","" );
+        workerPrint=workerPrint.replace("[","" );
+        workerPrint=workerPrint.replace("]","" );
+        workerPrint=workerPrint.replace(",","" );
+        return workerPrint;}
+
 
     public static void storageToString(Employee[] storage) {
         for (int i = 0; i < storage.length; i++) {
@@ -10,10 +17,10 @@ public class CourseWork {
         }
     }
 
-    public static String printName(Employee[] storage){
-        String[] printWorker=new String[storage.length];
+    public static String printName(Employee [] storage){
+         String[] printWorker=new String[storage.length];
         for (int i=0;i<storage.length;i++){
-            printWorker[i]= storage[i].getWorker();}
+            printWorker[i]=storage[i].getWorker();}
         String workerPrint=Arrays.toString(printWorker);
         workerPrint=workerPrint.replace("[","" );
         workerPrint=workerPrint.replace("]","" );
@@ -26,30 +33,31 @@ public class CourseWork {
         return salaryMonth;
     }
 
-    public static int MinSalary(Employee[] storage) {
-        int salaryTotal = 0;
-        int salaryMin=0;
-        for (int i = 0; i < storage.length; i++) {
-            salaryTotal += storage[i].getEmployeeSalary();
-            salaryMin=salaryTotal;}
-        for (int i = 0; i < storage.length; i++){
-            if (storage[i].getEmployeeSalary() < salaryMin)
-                salaryMin = storage[i].getEmployeeSalary();}
-        return salaryMin;
-    }
+    public static String MinSalary(Employee[] storage) {
+        int salaryMin=monthSalary(storage);
+        String workerMin="";
+        for ( int i = 0; i < storage.length-1; i++){
+            if (salaryMin>storage[i].getEmployeeSalary()){
+             salaryMin=storage[i].getEmployeeSalary();}if (salaryMin==storage[i].getEmployeeSalary())
+            workerMin=storage[i].getWorker()+storage[i].getEmployeeSalary();}
+        return workerMin;}
 
-    public static int MaxSalary(Employee[] storage) {
+
+    public static String MaxSalary(Employee[] storage) {
         int salaryMax=-1;
+        String workerMax="";
         for (int i = 0; i < storage.length; i++){
-            if (storage[i].getEmployeeSalary() > salaryMax)
-                salaryMax = storage[i].getEmployeeSalary();}
-        return salaryMax;
+            if (storage[i].getEmployeeSalary() > salaryMax){
+                salaryMax=storage[i].getEmployeeSalary();}
+            {if (salaryMax==storage[i].getEmployeeSalary())
+            workerMax=storage[i].getWorker()+storage[i].getEmployeeSalary();}}
+        return workerMax;
     }
 
-    public static int averageSalary(Employee[] storage){
+    public static double averageSalary(Employee[] storage){
         int salaryMonth=0;
         for (int i=0;i<storage.length;i++){salaryMonth+=storage[i].getEmployeeSalary();}
-        return salaryMonth/storage.length;
+        return (double)salaryMonth/storage.length;
     }
 
     public static  Employee [] indexSalary(Employee[] storage){
@@ -57,27 +65,29 @@ public class CourseWork {
         return storage;
     }
 
-    public static int MinDeptSalary(Employee[] storage,int x) {
-        int salaryTotal = 0;
-        int salaryMin = 0;
-        for (int i = 0; i < storage.length; i++) {
-            salaryTotal += storage[i].getEmployeeSalary();
-            salaryMin = salaryTotal;}
+    public static String MinDeptSalary(Employee[] storage,int x) {
+        int salaryMin=monthSalary(storage);
+        String workerMin="";
         for (int i = 0; i < storage.length; i++) {
             if (storage[i].getEmployDept() == x) {
                 if (storage[i].getEmployeeSalary() < salaryMin) {
-                    salaryMin = storage[i].getEmployeeSalary();}
+                    if (salaryMin>storage[i].getEmployeeSalary()){
+                        salaryMin=storage[i].getEmployeeSalary();}if (salaryMin==storage[i].getEmployeeSalary())
+                        workerMin=storage[i].getWorker()+storage[i].getEmployeeSalary();} }
                 }
-        }return salaryMin;}
+        return workerMin;}
 
-    public static int MaxDeptSalary(Employee[] storage,int x) {
+    public static String MaxDeptSalary(Employee[] storage,int x) {
         int salaryMax=-1;
+        String workerMax="";
         for (int i = 0; i < storage.length; i++) {
             if (storage[i].getEmployDept() == x) {
-                if (storage[i].getEmployeeSalary() > salaryMax) {
-                    salaryMax = storage[i].getEmployeeSalary();
-                } }
-        }return salaryMax;}
+                if (storage[i].getEmployeeSalary() > salaryMax){
+                    salaryMax=storage[i].getEmployeeSalary();}
+                {if (salaryMax==storage[i].getEmployeeSalary())
+                    workerMax=storage[i].getWorker()+storage[i].getEmployeeSalary();}}}
+
+        return workerMax;}
 
     public static int monthDeptSalary(Employee[] storage,int x){
         int salaryMonth=0;
@@ -86,7 +96,7 @@ public class CourseWork {
         return salaryMonth;
     }
 
-    public static int averageDeptSalary(Employee[] storage,int x){
+    public static double averageDeptSalary(Employee[] storage,int x){
         int salaryMonth=0;
         int [] v=new int[storage.length];
         int a=0;
@@ -94,7 +104,7 @@ public class CourseWork {
         for (int i=0;i<storage.length;i++){if (storage[i].getEmployDept() == x){
             salaryMonth+=storage[i].getEmployeeSalary();
             v[i]=storage[i].getEmployeeSalary();if(v[i]!=0){b=b+1;}}
-            }return salaryMonth/b;}
+            }return (double)salaryMonth/b;}
 
     public static  Employee [] indexDeptSalary(Employee[] storage,int x,int a){
         for (int i=0;i<storage.length;i++){if (storage[i].getEmployDept() == x)
@@ -105,38 +115,25 @@ public class CourseWork {
         int b=0;
         String [] v=new String[storage.length];
         String[] printWorker=new String[b];
-        //int[] printSalary=new int[b];
         for (int i=0;i<storage.length;i++) {
             if (storage[i].getEmployDept() == x) {
                 v[i]=storage[i].getWorker()+storage[i].getEmployeeSalary();if(v[i]!=null){b=b+1;}}}
         String workerPrint=Arrays.toString(v);
-        workerPrint=workerPrint.replace("null","" );
-        workerPrint=workerPrint.replace("[","" );
-        workerPrint=workerPrint.replace("]","" );
-        workerPrint=workerPrint.replace(",","" );
-        return workerPrint;}
+        return StringToPrint(workerPrint);}
 
     public static String MinWorkSalary(Employee[] storage,int x){
         String [] v=new String[storage.length];
         for(int i=0;i<storage.length;i++){if(storage[i].getEmployeeSalary()<x)
         {v[i]=storage[i].getId()+storage[i].getWorker()+storage[i].getEmployeeSalary();}}
         String workerPrint=Arrays.toString(v);
-        workerPrint=workerPrint.replace("null","" );
-        workerPrint=workerPrint.replace("[","" );
-        workerPrint=workerPrint.replace("]","" );
-        workerPrint=workerPrint.replace(",","" );
-        return workerPrint;}
+        return StringToPrint(workerPrint);}
 
     public static String MaxWorkSalary(Employee[] storage,int x){
         String [] v=new String[storage.length];
         for(int i=0;i<storage.length;i++){if(storage[i].getEmployeeSalary()>=x)
         {v[i]=storage[i].getId()+storage[i].getWorker()+storage[i].getEmployeeSalary();}}
         String workerPrint=Arrays.toString(v);
-        workerPrint=workerPrint.replace("null","" );
-        workerPrint=workerPrint.replace("[","" );
-        workerPrint=workerPrint.replace("]","" );
-        workerPrint=workerPrint.replace(",","" );
-        return workerPrint;}
+        return StringToPrint(workerPrint);}
 
 
 
@@ -157,30 +154,22 @@ public class CourseWork {
                 storage[11] = new Employee(" Уламбек Нуруло ", 5, 100000);
 
 
-
-                System.out.println(printName(storage));
+                storageToString(storage);
                 System.out.println(monthSalary(storage));
                 System.out.println(MinSalary(storage));
                 System.out.println(MaxSalary(storage));
                 System.out.println(averageSalary(storage));
-
+                System.out.println(printName(storage));
                 indexSalary(storage);
-                storageToString(storage);
-                int x = 1;
-                for (int i = 0; i < storage.length; i++) {
-                    if (storage[i].getEmployDept() == x) {
-                        System.out.println(storage[i]);
-                        ;
-                    } else break;
-                }
+
+
                 System.out.println(MinDeptSalary(storage,5));
                 System.out.println(MaxDeptSalary(storage, 5));
                 System.out.println(monthDeptSalary(storage, 2));
                 System.out.println(averageDeptSalary(storage, 2));
 
                 indexDeptSalary(storage,5,10);
-                storageToString(storage);
-                System.out.println(printDeptName(storage, 4));
+                System.out.println(printDeptName(storage, 5));
 
 
                 System.out.println(MinWorkSalary(storage, 50000));
